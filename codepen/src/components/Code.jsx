ca9b2ae796c92e4ffa34936633cf72852f56b25a
Shubhi Agrawal <shubhi.agrawal_cs19@gla.ac.in>
2024-02-20 16:05:43 +0530

    codemirror not working

--- codepen/src/components/Code.jsx
+++ codepen/src/components/Code.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Editor from './Editor';
+import { DataContext } from '../context/DataProvider';
+import { Box, styled } from '@mui/material';
+
+const Container = styled(Box)`
+    background-color: #060606;
+    height: 50vh;
+    display: flex;
+`
 
 const Code = () => {
+
+  const { html, css, js, setHtml, setCss, setJs } = useContext(DataContext);
+
   return (
     <>
-      <Editor />
-      <Editor />
-      <Editor />
+      <Container>
+            <Editor 
+                language="xml"
+                heading="HTML"
+                value={html}
+                onChange={setHtml}
+                icon='/'
+                color='#FF3C41'
+            />
+            <Editor 
+                language="css"
+                heading="CSS"
+                value={css}
+                onChange={setCss}
+                icon='*'
+                color='#0EBEFF'
+            />
+            <Editor 
+                language="javascript"
+                heading="JS"
+                value={js}
+                onChange={setJs}
+                icon='( )'
+                color='#FCD000'
+            />
+        </Container>
     </>
   )
 }
--- codepen/src/components/Editor.jsx
+++ codepen/src/components/Editor.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
-import { Box, styled } from '@mui/material';
-import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+import React, { useState } from 'react';
+import '../App.css';
+import { Box, Typography, Button, styled } from '@mui/material';
+import CloseFullscreen from '@mui/icons-material/CloseFullscreen';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+import 'react-codemirror/node_modules/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
+
+const Container = styled(Box)`
+    flex-grow: 1;
+    flex-basic: 0;
+    display: flex;
+    flex-direction: column;
+    padding: 0 8px 8px;
+`;
 
 const Heading = styled(Box)`
   background: #1e1d22;
@@ -16,32 +31,56 @@ const Header = styled(Box)`
   font-weight: 700;
 `;
 
-const Editor = () => {
+const Editor = ({ heading, language, value, onChange, icon, color }) => {
+
+  const [open, setOpen] = useState(true);
+
+  const handleChange = (editor, data, value) => {
+      onChange(value);
+  }
+
   return (
     <>
-        <Box>
-          <Header>
-            <Heading>
-              <Box
-               component='span'
-               style={{
-                background: 'red',
-                height:  20,
-                width: 20,
-                borderRadius: 5,
-                display: 'flex',
-                placeContent: 'center',
-                marginRight: 5,
-                padding: 2,
-               }}
-               >
-                /  
-              </Box>
-              HTML
-            </Heading>
-            <CloseFullscreenIcon />
-          </Header>
-        </Box>
+        <Container style={open ? null : {flexGrow: 0} }>
+            <Header>
+                <Heading>
+                    <Box 
+                        component="span"
+                        style={{
+                            background: color,
+                            borderRadius: 5,
+                            marginRight: 5,
+                            height: 20,
+                            width: 20,
+                            display: 'flex',
+                            placeContent: 'center',
+                            color: '#000',
+                            paddingBottom: 2
+                        }}
+                    >
+                        {icon}
+                    </Box>
+                    {heading}
+                </Heading>
+                <CloseFullscreen 
+                    fontSize="small"
+                    style={{ alignSelf: 'center'}}
+                    onClick={() => setOpen(prevState => !prevState)}
+                />
+            </Header>
+            <ControlledEditor 
+                onBeforeChange={handleChange}
+                value={value}
+                className="controlled-editor"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    lineNumbers: true,
+                    theme: 'material'
+                }}
+            />
+        </Container>
     </>
   );
 };
